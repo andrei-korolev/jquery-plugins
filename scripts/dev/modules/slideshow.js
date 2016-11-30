a34421577a498453db18e7c74187ac308c8b083e
Андрей <andrey-asd123@yandex.ru>
2016-11-30 07:52:14 +0300

    Changed module - slideshow

--- scripts/dev/modules/slideshow.js
+++ scripts/dev/modules/slideshow.js
@@ -6,30 +6,114 @@ let nc = window.nc || {},
 
 ncModules.slideshow = ( () => {
 
-    function init() {
-        initVars();
+    let myObj = {};
+
+
+    function init(element) {
+        initVars(element);
         bindEvents();
     }
     
     
-    function initVars() {}
+    function initVars(element) {
+        myObj.display = myObj.main.querySelector('.slideshow__display');
+        myObj.mainImage = myObj.display.querySelector('.slideshow__image');
+        myObj.list = myObj.main.querySelector('.slideshow__list');
+        myObj.duration = parseInt(getComputedStyle(myObj.mainImage).transitionDuration) * 1000;
+
+        changeImage = debounce(changeImage, myObj.duration);
+
+        console.log(myObj.duration);
+    }
 
     
     function bindEvents() {
         /*events*/
-        $('.block').on('click', onClick);
+        myObj.list.addEventListener('click', changeImage);
     }
 
 
-    init();
-    
+    return (element) => {
+        myObj.main = element;
 
-    /*Public Api*/
-    return {};
+        init();
+
+        /*Public Api*/
+        return {
+
+        }
+    };
 
     
     /*Helpers*/
-    function onClick() {}
 
+    /*To change the picture of the main container*/
+    function changeImage(e) {
+        let target = e.target;
+
+        myObj.item = target.closest('.slideshow__item');
+
+        if (!myObj.item || myObj.item === myObj.lastItem) {
+            return;
+        }
+
+        myObj.image = myObj.item.querySelector('.slideshow__image');
+        myObj.src = getNewSrc();
+        myObj.lastItem = myObj.item;
+
+        hideImage();
+    }
+
+
+    /*Get new src*/
+    function getNewSrc() {
+        return myObj.image.dataset.src || myObj.image.getAttribute('src');
+    }
+
+
+    /*Set new src*/
+    function setNewSrc() {
+        myObj.mainImage.src = myObj.src;
+    }
+
+
+    /*Hide image*/
+    function hideImage() {
+        myObj.mainImage.style.opacity = 0;
+
+        console.log("done hide");
+
+        setTimeout(() => {
+            setNewSrc();
+            showImage();
+            console.log(myObj.duration);
+        }, myObj.duration);
+    }
+
+
+    /*Show image*/
+    function showImage() {
+        myObj.mainImage.style.opacity = 1;
+        console.log("done show");
+    }
+
+
+    /*Debounce*/
+    function  debounce(f, time) {
+        let status = true;
+
+        return function() {
+
+            if (status) {
+                f.apply(this, arguments);
+
+                status = false;
+
+                setTimeout(() => {
+                    status = true;
+                }, time);
+            }
+        }
+    }
 
 })();
